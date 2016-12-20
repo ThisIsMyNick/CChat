b61096c3a578e24607eb7846bd9aa08a9cb3524f
ThisIsMyNick <nobel.gautam@gmail.com>
2016-12-20 10:07:40 -0500

    Add command line parsing

---
+++ src/client/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+
+void print_help()
+{
+    printf("Usage:\n"
+            "CChat (ip:port|name) [--nick yourname] [--help]\n");
+}
+
+int main(int argc, char *argv[])
+{
+    const char* const short_options = "hn:";
+    const struct option long_options[] =
+    {
+        {"help", 0, NULL, 'h'},
+        {"nick", 1, NULL, 'n'},
+        { NULL , 0, NULL,  0 }
+    };
+
+    int next_option;
+    while ((next_option = getopt_long(argc, argv, short_options, long_options, NULL)) != -1)
+    {
+        switch (next_option)
+        {
+            case 'h':
+                print_help();
+                break;
+            case 'n':
+                printf("Nickname\n");
+                break;
+            default:
+                print_help();
+        }
+    }
+
+}
