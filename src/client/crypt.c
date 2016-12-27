e16b38abe509369df8f8bcbe8deb9a16dcf0a316
James Wang <jameswang9909@hotmail.com>
2016-12-27 09:57:17 -0500

    Use AES CBC mode for encryption

--- src/client/crypt.c
+++ src/client/crypt.c
@@ -13,18 +13,20 @@ void generate_key(aes_t *buf) {
     }
 }
 
-aes_t *encrypt(aes_t *plaintext, aes_t *key) {
+aes_t *encrypt(char *plaintext, aes_t *key, aes_t *iv) {
     aes_t *ciphertext = (aes_t *) malloc(BUFFER_SIZE);
     AES_KEY wctx;
-    AES_set_encrypt_key(key, 128, &wctx);
-    AES_encrypt(plaintext, ciphertext, &wctx);
+    AES_set_encrypt_key(key, KEY_SIZE * 8, &wctx);
+    AES_cbc_encrypt((aes_t *)plaintext, ciphertext, BUFFER_SIZE, &wctx, iv, AES_ENCRYPT);
+    memset(iv, 0x00, AES_BLOCK_SIZE);
     return ciphertext;
 }
 
-aes_t *decrypt(aes_t *ciphertext, aes_t *key) {
+char *decrypt(aes_t *ciphertext, aes_t *key, aes_t *iv) {
     aes_t *plaintext = (aes_t *) malloc(BUFFER_SIZE);
     AES_KEY wctx;
-    AES_set_decrypt_key(key, 128, &wctx);
-    AES_decrypt(ciphertext, plaintext, &wctx);
-    return plaintext;
+    AES_set_decrypt_key(key, KEY_SIZE * 8, &wctx);
+    AES_cbc_encrypt(ciphertext, plaintext, BUFFER_SIZE, &wctx, iv, AES_DECRYPT);
+    memset(iv, 0x00, AES_BLOCK_SIZE);
+    return (char *) plaintext;
 }
--- src/client/crypt.h
+++ src/client/crypt.h
@@ -1,10 +1,10 @@
-#define BUFFER_SIZE 1024
-#define KEY_SIZE 16
+#define KEY_SIZE 32
+#define BUFFER_SIZE 256 // KEY_SIZE * 8
 
 typedef unsigned char aes_t;
 
 void generate_key(aes_t *buf);
-aes_t *encrypt(aes_t *plaintext, aes_t *key);
-aes_t *decrypt(aes_t *ciphertext, aes_t *key);
+aes_t *encrypt(char *plaintext, aes_t *key, aes_t *iv);
+char *decrypt(aes_t *ciphertext, aes_t *key, aes_t *iv);
 aes_t *to_unsigned(char *buf);
 char *to_signed(aes_t *buf);
