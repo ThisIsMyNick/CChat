6567699402e72c276b2c178207ec8c9907b4181e
James Wang <jameswang9909@hotmail.com>
2017-01-07 10:37:09 -0500

    Minor refactor

--- src/client/crypt.c
+++ src/client/crypt.c
@@ -15,18 +15,18 @@ void generate_bytes(aes_t *buf) {
 
 aes_t *encrypt(char *plaintext, aes_t *key, aes_t *iv) {
     aes_t *ciphertext = (aes_t *) malloc(BUFFER_SIZE);
-    AES_KEY wctx;
-    AES_set_encrypt_key(key, KEY_SIZE * 8, &wctx);
-    AES_cbc_encrypt((aes_t *)plaintext, ciphertext, strlen(plaintext), &wctx, iv, AES_ENCRYPT);
+    AES_KEY enc_key;
+    AES_set_encrypt_key(key, KEY_SIZE * 8, &enc_key);
+    AES_cbc_encrypt((aes_t *)plaintext, ciphertext, strlen(plaintext), &enc_key, iv, AES_ENCRYPT);
     memset(iv, 0x00, AES_BLOCK_SIZE);
     return ciphertext;
 }
 
 char *decrypt(aes_t *ciphertext, aes_t *key, aes_t *iv) {
     aes_t *plaintext = (aes_t *) malloc(BUFFER_SIZE);
-    AES_KEY wctx;
-    AES_set_decrypt_key(key, KEY_SIZE * 8, &wctx);
-    AES_cbc_encrypt(ciphertext, plaintext, strlen((char *)ciphertext), &wctx, iv, AES_DECRYPT);
+    AES_KEY dec_key;
+    AES_set_decrypt_key(key, KEY_SIZE * 8, &dec_key);
+    AES_cbc_encrypt(ciphertext, plaintext, strlen((char *)ciphertext), &dec_key, iv, AES_DECRYPT);
     memset(iv, 0x00, AES_BLOCK_SIZE);
     return (char *) plaintext;
 }
