2722e807a0e808cabbf8e39066f620d93ad342c0
James Wang <jameswang9909@hotmail.com>
2017-01-24 03:34:34 -0500

    Update window.h

--- src/client/window.h
+++ src/client/window.h
@@ -1,7 +1,11 @@
 #pragma once
 
+void title_bar();
+void draw_messages(msg *msg_list, int length, int display_to);
+int get_max_x();
+int draw_prompt();
+void clear_msgs();
 void update_window(msgs_data *d);
 void get_input(char *dst);
 void init_window();
 void close_window();
-int get_max_x();
